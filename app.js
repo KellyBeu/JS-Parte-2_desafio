1ac771c3df7e8e6405c08f1be4743050c89d3813
Kelly Beu <kelly.beu@gmail.com>
2025-01-27 14:16:36 -0300

    Desafio Git Commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 /*1. Crie uma função que calcule o índice de massa corporal (IMC) 
 de uma pessoa, a partir de sua altura, em metros, e peso, 
 em quilogramas, que serão recebidos como parâmetro.*/
---
+++ desafioGit.js
@@ -0,0 +1,20 @@
+/*Desafios Git / Git HUB
+Crie uma conta no GitHub (ok)
+Crie um repositório para um projeto pessoal. (ok)
+Faça a instalação do Git (ok)
+Crie um repositório localmente para o seu projeto pessoal (ok)
+Adicione alguns arquivos no seu repositório local (ok)
+Faça o commit das alterações
+Configure a identidade do autor do commit.
+Crie a branch Main
+Realize a conexão do seu repositório local com o remoto
+Envie as alterações no repositório local para o remoto
+Utilize o comando git status*/
+
+
+console.log('Desafio do Git');
+let nome =  prompt('Digite o seu nome:');
+console.log(nome);
+
+alert('Seja bem vinda(o) ' + nome + ', ao desafio do Git/Git Hub!!!');
+
